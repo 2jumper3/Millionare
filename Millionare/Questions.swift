3934b6fb8422dc5ab75350ede1704c25acd0f13e
2jumper3 <2jumper3@gmail.com>
2019-10-22 16:09:47 +0300

    question

---
+++ Millionare/Questions.swift
@@ -0,0 +1,21 @@
+//
+//  Questions.swift
+//  Millionare
+//
+//  Created by Sergey on 22.10.2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import SwiftUI
+
+struct Questions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Questions_Previews: PreviewProvider {
+    static var previews: some View {
+        Questions()
+    }
+}
---
+++ Millionare/questionView.swift
@@ -0,0 +1,21 @@
+//
+//  questionView.swift
+//  Millionare
+//
+//  Created by Sergey on 22.10.2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import SwiftUI
+
+struct questionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct questionView_Previews: PreviewProvider {
+    static var previews: some View {
+        questionView()
+    }
+}
