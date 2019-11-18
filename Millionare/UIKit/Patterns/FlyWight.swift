bb866465acb3629ff3cfb7bc8a50cfda52a78b76
2jumper3 <2jumper3@gmail.com>
2019-11-18 12:04:22 +0300

    testCommit

---
+++ Millionare/UIKit/Patterns/FlyWight.swift
@@ -0,0 +1,9 @@
+//
+//  FlyWight.swift
+//  Millionare
+//
+//  Created by Sergey on 15.11.2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import Foundation
---
+++ Millionare/UIKit/Patterns/Prototype.swift
@@ -0,0 +1,9 @@
+//
+//  Prototype.swift
+//  Millionare
+//
+//  Created by Sergey on 15.11.2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import Foundation
